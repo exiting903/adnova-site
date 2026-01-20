6111cdcd390996dc6ba4eac2a8361f1d9784bc7e
Mukhammad Aziz <118566435+exiting903@users.noreply.github.com>
2026-01-20 09:59:39 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -89,9 +89,8 @@ const reviewsData = [
 
 const Logo = () => (
   <div className="flex items-center gap-2">
-    <img src="/logoo.PNG" alt="Logo" className="h-10 w-auto object-contain" />
+    <img src="/logoo.PNG" alt="Logo" className="h-11 w-auto object-contain" />
     
-    <span className="font-header font-bold text-lg tracking-widest text-black"></span>
   </div>
 );
 
