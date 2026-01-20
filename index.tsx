f9efb74aff65ca7a91a4f3427d8eb7ab432ca997
Mukhammad Aziz <118566435+exiting903@users.noreply.github.com>
2026-01-20 13:21:27 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -89,7 +89,7 @@ const reviewsData = [
 
 const Logo = () => (
   <div className="flex items-center gap-2">
-    <img src="/logoo.PNG" alt="Logo" className="h-28 w-auto object-contain -ml-1" />
+    <img src="/logoo.PNG" alt="Logo" className="h-28 w-auto object-contain -ml-3" />
     
   </div>
 );
